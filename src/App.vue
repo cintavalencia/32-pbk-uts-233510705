e8e2a1a96476ba0f76412ce99922be151d70f3e0
cintavalencia <valenciacinta072@gmail.com>
2025-05-01 13:48:28 +0700

    add item

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <script setup>
+import { ref } from 'vue';
+
+const items = ref([]);
+const newItem = ref('');
+
+const addItem = () => {
+  if (newItem.value !== '') {
+    items.value.push({
+      id: items.value.length + 1,
+      text: newItem.value,
+      completed: false
+    });
+    newItem.value = '';
+    console.log(items.value);
+  }
+};
 </script>
 
 <template>
   <h1 class="text-red-500">todo list</h1>
+  <input type="text" v-model="newItem" @keyup.enter="addItem" />
+  <button @click="addItem">Tambah</button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
